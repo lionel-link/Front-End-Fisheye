89790c7c216b119050759ddf0686a412c65ef96f
lionel-link <lioneladje@gmail.com>
2023-04-09 13:26:32 +0200

    Update getData.js

--- docs/scripts/utils/getData.js
+++ docs/scripts/utils/getData.js
@@ -1,7 +1,12 @@
 export async function getData() {
   let data = [];
   if (data.length === 0) {
-    await fetch("./../../docs/data/photographers.json")
+    await fetch("./../../docs/data/photographers.json", {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
       .then((response) => response.json())
       .then((response)  => data = response);
   }
