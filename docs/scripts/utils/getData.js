de2e61d40db4a8d5a1c4922b0597ea58432d5fdb
lionel-link <53856679+lionel-link@users.noreply.github.com>
2023-04-09 12:47:09 +0200

    Update getData.js

--- docs/scripts/utils/getData.js
+++ docs/scripts/utils/getData.js
@@ -1,7 +1,7 @@
 export async function getData() {
   let data = [];
   if (data.length === 0) {
-    await fetch("./../../data/photographers.json")
+    await fetch("https://github.com/lionel-link/Front-End-Fisheye/blob/main/docs/data/photographers.json")
       .then((response) => response.json())
       .then((response)  => data = response);
   }
